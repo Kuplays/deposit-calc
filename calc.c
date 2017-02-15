63106b1a3d03f1436619f025681792eb1dc68606
Куплайс Александр Олегович <ip614s10@mwp04.csc.local>
2017-02-15 14:45:47 +0700

    Implement user input validation

--- calc.c
+++ calc.c
@@ -6,7 +6,19 @@ int main(void) {
     
     printf("Enter investment duration: ");
     scanf("%d", &duration);
+
+    if (duration < 0 || duration > 365 ) {
+        printf("Invalid duration time, aborting....\n");
+        return 1;
+    }
     printf("Enter investent amount: ");
     scanf("%f", &investment);
+
+    if (investment < 0.0) {
+        printf("Invalid investment amount, aborting....\n");
+        return 1;
+    }
     printf("%d %f", duration, investment);
+    
+    return 0;
 }
