6b5ae10457852e670355d87db5e13122c58d06d7
Куплайс Александр Олегович <ip614s10@mwp04.csc.local>
2017-02-15 14:59:28 +0700

    Implement investment percentage

--- calc.c
+++ calc.c
@@ -18,6 +18,37 @@ int main(void) {
         printf("Invalid investment amount, aborting....\n");
         return 1;
     }
+    
+    if (duration >= 0 && duration <= 30) {
+        investment *= 0.9;
+    }
+
+    if (investment < 100000) {
+        if (duration >= 31 && duration <= 120) {
+            investment *= 1.02;
+        }
+
+        if (duration >= 121 && duration <= 240) {
+            investment *= 1.06;
+        }
+        if (duration >= 241 && duration <= 365) {
+            investment *= 1.12;
+        }
+    }
+
+    if (investment > 100000) {
+        if (duration >= 31 && duration <= 120) {
+            investment *= 1.03;
+        }
+
+        if (duration >= 121 && duration <= 240) {
+            investment *= 1.08;
+        }
+        if (duration >= 241 && duration <= 365) {
+            investment *= 1.15;
+        }
+    }
+
     printf("%d %f", duration, investment);
     
     return 0;
