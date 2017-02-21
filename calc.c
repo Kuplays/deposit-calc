5ea90db31cb89b12ad21d045618908f878d4164d
Aleks Kuplais <alexkuplays@gmail.com>
2017-02-21 19:26:09 +0600

    Implement User Input Function

--- calc.c
+++ calc.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+void getInput(int*, float*);
+void perform(int*, float*);
+
 int main(void) {
     int duration;
     float investment;
@@ -53,4 +56,14 @@ int main(void) {
     printf("\n");
     
     return 0;
+}
+
+void getInput(int* dur, float* amount) {
+    printf("Enter investment duration: ");
+    scanf("%d", dur);
+    printf("Enter investent amount: ");
+    scanf("%f", amount);
+    if (*dur < 0 || *dur > 365 || *amount < 0.0) {
+        getInput(dur, amount);
+    }
 }
